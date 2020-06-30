4a86f6100916e9642ba5298fcae099364c0124a4
spencerrhea <spenrhea@rams.colostate.edu>
2020-06-30 09:44:36 -0600

    "not many changes"

--- src/acquisition_master.R
+++ src/acquisition_master.R
@@ -38,7 +38,7 @@ network_domain = sm(read_csv('data/general/site_data.csv')) %>%
 
 ms_globals = c(ls(all.names=TRUE), 'email_err_msgs')
 
-# dmnrow=1
+ dmnrow=1
 for(dmnrow in 1:nrow(network_domain)){
 
     network = network_domain$network[dmnrow]
