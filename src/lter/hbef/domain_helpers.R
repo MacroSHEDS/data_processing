066a7d7c4a446fc71f917cdfc62563a23dda3077
Mike Vlah <vlahm13@gmail.com>
2020-06-29 21:03:02 -0400

    revising hbef stream/precip chem retrieval and munge

--- src/lter/hbef/domain_helpers.R
+++ src/lter/hbef/domain_helpers.R
@@ -1,7 +1,7 @@
 
 #. handle_errors
 munge_hbef_site <- function(domain, site, prodname_ms, tracker, silent=TRUE){
-     #ite=sites[1]; tracker=held_data; k=1
+    # site=sites[j]; tracker=held_data; k=1
 
     retrieval_log = extract_retrieval_log(held_data, prodname_ms, site)
 
@@ -59,63 +59,63 @@ munge_hbef_site <- function(domain, site, prodname_ms, tracker, silent=TRUE){
 #. handle_errors
 munge_hbef_combined <- function(domain, site, prodname_ms, tracker, prodcode, silent=TRUE){
     #site=sites[1]; tracker=held_data; k=1
-    
+
     retrieval_log = extract_retrieval_log(held_data, prodname_ms, site) %>%
         filter(component != "Analytical_Methods.csv")
-    
+
     if(nrow(retrieval_log) == 0){
         return(generate_ms_err('missing retrieval log'))
     }
-    
+
     out = tibble()
     for(k in 1:nrow(retrieval_log)){
-        
+
         processing_func = get(paste0('process_1_', prodcode))
         in_comp = pull(retrieval_log[k, 'component'])
-        
+
         out_comp = sw(do.call(processing_func,
             args=list(network=network, domain=domain, prodname_ms=prodname_ms,
                 site_name=site, component=in_comp)))
-        
+
         if(! is_ms_err(out_comp) && ! is_ms_exception(out_comp)){
             out = bind_rows(out, out_comp)
         }
-    
+
     gauges <- unique(out_comp$site_name)
-    
+
     for(f in 1:length(gauges)) {
         new <- out_comp %>%
             filter(site_name == gauges[f])
-        
+
         prod_dir = glue('data/{n}/{d}/munged/{p}', n=network, d=domain,
             p=prodname_ms)
         dir.create(prod_dir, showWarnings=FALSE, recursive=TRUE)
         site_file = glue('{pd}/{s}.feather', pd=prod_dir, s=gauges[f])
         write_feather(new, site_file)
-        
+
         portal_prod_dir = glue('../portal/data/{d}/{p}',
             d=domain, p=strsplit(prodname_ms, '_')[[1]][1])
         dir.create(portal_prod_dir, showWarnings=FALSE, recursive=TRUE)
         portal_site_file = glue('{pd}/{s}.feather', pd=portal_prod_dir, s=gauges[f])
-        
+
         #if there's already a data file for this site-time-product in the portal
         #repo, remove it
         unlink(portal_site_file)
-        
+
         #create a link to the new file from the portal repo
         #(from and to seem logically reversed in file.link)
         invisible(sw(file.link(to=portal_site_file, from=site_file)))
     }
     }
-    
+
     update_data_tracker_m(network=network, domain=domain,
         tracker_name='held_data', prodname_ms=prodname_ms, site=site,
         new_status='ok')
-    
+
     msg = glue('munged {p} ({n}/{d}/{s})',
         p=prodname_ms, n=network, d=domain, s=site)
     loginfo(msg, logger=logger_module)
-    
+
     return('sitemunge complete')
 }
 
--- src/lter/hbef/processing_kernels.R
+++ src/lter/hbef/processing_kernels.R
@@ -21,7 +21,7 @@ process_0_13 <- function(set_details, network, domain){
         s=set_details$site_name)
     dir.create(raw_data_dest, showWarnings=FALSE, recursive=TRUE)
     download.file(url=set_details$url,
-        destfile=glue(raw_data_dest, '/', set_details$component),
+        destfile=glue(raw_data_dest, '/', set_details$component, '.csv'),
         cacheOK=FALSE, method='curl')
 }
 
@@ -33,8 +33,8 @@ process_0_208 <- function(set_details, network, domain){
         s=set_details$site_name)
     dir.create(raw_data_dest, showWarnings=FALSE, recursive=TRUE)
     download.file(url=set_details$url,
-        destfile=glue(raw_data_dest, '/', set_details$component),
-        cacheOK=FALSE, method='curl') 
+        destfile=glue(raw_data_dest, '/', set_details$component, '.csv'),
+        cacheOK=FALSE, method='curl')
 }
 
 #rain_gauge_locations: STATUS=PENDING (not sure why this cannot be accessed, may it does not exist. also repeated with 100)
@@ -111,10 +111,10 @@ process_1_1 <- function(network, domain, prodname_ms, site_name,
 #. handle_errors
 process_1_13 <- function(network, domain, prodname_ms, site_name,
     component){
-    
+
     rawfile = glue('data/{n}/{d}/raw/{p}/{s}/{c}',
         n=network, d=domain, p=prodname_ms, s=site_name, c=component)
-    
+
     d = sw(read_csv(rawfile, progress=FALSE,
         col_types=readr::cols_only(
             DATE='c', #can't parse 24:00
@@ -138,95 +138,38 @@ process_1_13 <- function(network, domain, prodname_ms, site_name,
 #. handle_errors
 process_1_208 <- function(network, domain, prodname_ms, site_name,
     component){
-    
+
     rawfile = glue('data/{n}/{d}/raw/{p}/{s}/{c}',
         n=network, d=domain, p=prodname_ms, s=site_name, c=component)
 
     if(component == "precipitation_chemistry.csv") {
-    
-    d = sw(read_csv(rawfile, col_types=readr::cols_only(
-        site='c', 
-        date='D',
-        timeEST='t',
-        pH = "n",
-        DIC = "n",
-        spCond = "n",
-        temp = "n",
-        ANC960 = "n",
-        ANCMet = "n",
-        precipCatch = "n",
-        notes = "c",
-        Ca = "n",
-        Mh = "n",
-        K = "n",
-        Na = "n",
-        TMAl = "n",
-        OMAl = "n",
-        Al_ICP = "n",
-        NH4 = "n",
-        SO4 = "n",
-        NO3 = "n",
-        Cl = "n",
-        PO4 = "n",
-        DOC = "n",
-        TDN = "n",
-        DON = "n",
-        SiO2 = "n",
-        Mn = "n",
-        Fe = "n",
-        F = "n", 
-        cationCharge = "n",
-        feildCode = "c",
-        anionCharge = "n", 
-        theoryCond = "n",
-        ionError = "n",
-        ionBalance = "n"))) %>%
-        mutate(datetime = ifelse(is.na(timeEST), paste(date, "00:00:00", " "), 
-            paste(date, timeEST, " "))) %>%
-        mutate(datetime = force_tz(ymd_hms(datetime), 'US/Eastern'),
-            site = ifelse(site %in% c("W1", "W2", "W3", "W4", "W5", "W6", "W7", "W8", "W9"), 
-                tolower(site), site)) %>%
+
+    d <- sw(read_csv(rawfile, col_types=readr::cols_only(
+            site='c', date='D', timeEST='t', pH='n', DIC='n', spCond='n',
+            temp='n', ANC960='n', ANCMet='n', precipCatch='n',# notes='c',
+            Ca='n', Mg='n', K='n', Na='n', TMAl='n', OMAl='n',
+            Al_ICP='n', NH4='n', SO4='n', NO3='n', Cl='n', PO4='n',
+            DOC='n', TDN='n', DON='n', SiO2='n', Mn='n', Fe='n',
+            `F`='n', cationCharge='n', fieldCode='c', anionCharge='n',
+            theoryCond='n', ionError='n', ionBalance='n'))) %>%
+        mutate(
+            timeEST = ifelse(is.na(timeEST), '12:00:00', timeEST),
+            datetime = ymd_hms(paste(date, timeEST), tz = 'UTC'),
+            ms_status = ifelse(is.na(fieldCode), FALSE, TRUE)) %>% #see summarize
         # can't find metadata on DIC, spCond, ANC960, DON, cationCharge, anionCharge, theoryCond, ionBalance
         #spCond is in ms/m but variables tables says S/cm which seems less common
-        rename(PO4_P = PO4,
-            site_name = site) %>%
-        select(-date, timeEST, -ionError) %>%
-        # notes of 1, 2 bad. (not posative these are correct)
+        rename(site_name = site) %>%
+        #convert to N, P
+        select(-date, -timeEST, -PO4, -NH4, -NO3, -fieldCode) %>%
         group_by(datetime, site_name) %>%
-        summarize(
-            pH = mean(pH, na.rm = TRUE),
-            DIC = mean(DIC, na.rm = TRUE),
-            spCond = mean(spCond, na.rm = TRUE),
-            temp = mean(temp, na.rm = TRUE),
-            ANC960 = mean(ANC960, na.rm = T),
-            ANCMet = mean(ANCMet, na.rm = TRUE),
-            precipCatch = mean(precipCatch, na.rm = TRUE), 
-            Ca = mean(Ca, na.rm = TRUE),
-            K = mean(K, na.rm = TRUE),
-            Na = mean(Na, na.rm = TRUE),
-            TMAl = mean(TMAl, na.rm = TRUE),
-            OMAl = mean(OMAl, na.rm = TRUE),
-            Al_ICP = mean(Al_ICP, na.rm = TRUE),
-            NH4 = mean(NH4, na.rm = TRUE),
-            SO4 = mean(SO4, na.rm = TRUE),
-            NO3 = mean(NO3, na.rm = TRUE),
-            Cl = mean(Cl, na.rm = TRUE),
-            PO4_P = mean(PO4_P, na.rm = TRUE),
-            DOC = mean(DOC, na.rm = TRUE),
-            TDN = mean(TDN, na.rm = TRUE),
-            DON = mean(DON, na.rm = TRUE),
-            SiO2 = mean(SiO2, na.rm = TRUE),
-            Mn = mean(Mn, na.rm = TRUE),
-            Fe = mean(Fe, na.rm = TRUE),
-            cationCharge = mean(cationCharge, na.rm = TRUE),
-            anionCharge = mean(anionCharge, na.rm = TRUE),
-            theoryCond = mean(theoryCond, na.rm = TRUE),
-            ionBalance = mean(ionBalance, na.rm = TRUE)) %>%
-        ungroup()
-    } 
+        summarize_all(~ if(is.numeric(.)) mean(., na.rm=TRUE) else any(.)) %>%
+        # summarize_if(is.numeric, mean, na.rm=TRUE) %>%
+        ungroup() %>%
+        mutate(ms_status = as.numeric(ms_status))
+    }
     if(component == "stream_chemistry.csv") {
             d = sw(read_csv(rawfile, col_types=readr::cols_only(
-                site='c', 
+                site='c',
                 date='D',
                 timeEST='t',
                 pH = "n",
@@ -254,17 +197,17 @@ process_1_208 <- function(network, domain, prodname_ms, site_name,
                 SiO2 = "n",
                 Mn = "n",
                 Fe = "n",
-                F = "n", 
+                F = "n",
                 cationCharge = "n",
                 feildCode = "c",
-                anionCharge = "n", 
+                anionCharge = "n",
                 theoryCond = "n",
                 ionError = "n",
                 ionBalance = "n"))) %>%
-                mutate(datetime = ifelse(is.na(timeEST), paste(date, "00:00:00", " "), 
+                mutate(datetime = ifelse(is.na(timeEST), paste(date, "00:00:00", " "),
                     paste(date, timeEST, " "))) %>%
                 mutate(datetime = force_tz(ymd_hms(datetime), 'US/Eastern'),
-                    site = ifelse(site %in% c("W1", "W2", "W3", "W4", "W5", "W6", "W7", "W8", "W9"), 
+                    site = ifelse(site %in% c("W1", "W2", "W3", "W4", "W5", "W6", "W7", "W8", "W9"),
                         tolower(site), site)) %>%
                 # can't find metadata on DIC, spCond, ANC960, DON, cationCharge, anionCharge, theoryCond, ionBalance
                 #spCond is in ms/m but variables tables says S/cm which seems less common
@@ -303,7 +246,34 @@ process_1_208 <- function(network, domain, prodname_ms, site_name,
                     ionBalance = mean(ionBalance, na.rm = TRUE)) %>%
                 ungroup()
     }
-    
+
     return(d)
 }
+
+process_1_208 <- function(network, domain, prodname_ms, site_name,
+    component){
+
+    rawfile = glue('data/{n}/{d}/raw/{p}/{s}/{c}',
+        n=network, d=domain, p=prodname_ms, s=site_name, c=component)
+
+    d = sw(read_csv(rawfile, progress=FALSE,
+        col_types=readr::cols_only(
+            DATE='c', #can't parse 24:00
+            rainGage='c',
+            Precip='d'))) %>%
+        # Flag='c'))) %>% #all flags are acceptable for this product
+        rename(datetime = DATE,
+            site_name = rainGage,
+            precip = Precip) %>%
+        mutate(
+            datetime = with_tz(force_tz(as.POSIXct(datetime), 'US/Eastern'), 'UTC'),
+            ms_status = 0) %>%
+        group_by(datetime, site_name) %>%
+        summarize(
+            precip = mean(precip, na.rm=TRUE),
+            ms_status = numeric_any(ms_status)) %>%
+        ungroup()
+}
+
+#derive kernels####
 
