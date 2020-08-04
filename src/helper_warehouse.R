eba244c6860396b744eadad7f67befdc963270b7
Mike Vlah <vlahm13@gmail.com>
2020-08-04 14:01:37 -0600

    new file location: src/global; new file: src/global/munge_engines.R

--- src/helper_warehouse.R
+++ src/helper_warehouse.R
@@ -75,3 +75,28 @@ populate_missing_shiny_files = function(domain){
     write_feather(qq, 'data/neon/precip.feather')
 
 }
+
+#maybe some useful parts for a web scraping function
+scrape_web <- function(){
+
+    require(rvest)
+    require(R.matlab)
+
+    setwd('~/git/macrosheds/data_acquisition/data/lter/hjandrews')
+
+    dset_urls = list(q=paste0('https://andrewsforest.oregonstate.edu/sites',
+        '/default/files/lter/data/weather/portal/MISC/DISCHARGE/data/index.html'))
+
+    for(i in 1:length(dset_urls)){
+        read_html(dset_urls[[i]]) %>%
+            html_node('td.title') %>%
+            html_text()
+    }
+
+    # d = readMat('https://andrewsforest.oregonstate.edu/sites/default/files/lter/data/weather/portal/MISC/DISCHARGE/data/discharge_5min_merged.mat')
+    for(i in 1:length(dset_urls)){
+        d = download.file('https://andrewsforest.oregonstate.edu/sites/default/files/lter/data/weather/portal/MISC/DISCHARGE/data/discharge_5min_merged.mat',
+            'provisional/q_merged.mat')
+        m = readMat('provisional/q_merged.mat')
+    }
+}
--- src/neon/neon/munge.R
+++ src/neon/neon/munge.R
@@ -33,22 +33,22 @@ for(i in 1:nrow(prod_info)){
                          s=site_name, p=prodname_ms), logger=logger_module)
         }
 
-        munge_msg = munge_neon_site(domain, sites[j], prodname_ms, held_data)
+        munge_rtn = munge_neon_site(domain, sites[j], prodname_ms, held_data)
 
-        if(is_ms_err(munge_msg)){
+        if(is_ms_err(munge_rtn)){
             update_data_tracker_m(network=network, domain=domain,
                 tracker_name='held_data', prodname_ms=prodname_ms, site_name=sites[j],
                 new_status='error')
         } else {
 
-            if(! is.na(prod_info$derive_status[i])){
-                update_data_tracker_d(network = network,
-                                      domain = domain,
-                                      tracker_name = 'held_data',
-                                      prodname_ms = prodname_ms,
-                                      site_name = site_name,
-                                      new_status = 'pending')
-            }
+           # if(! is.na(prod_info$derive_status[i])){
+           #     update_data_tracker_d(network = network,
+           #                           domain = domain,
+           #                           tracker_name = 'held_data',
+           #                           prodname_ms = prodname_ms,
+           #                           site_name = site_name,
+           #                           new_status = 'pending')
+           # }
 
             invalidate_derived_products(successor_string = prod_info$precursor_of)
         }
