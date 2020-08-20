1bd3ddf0f4f8074a915e1217ce4b9df22577de84
Mike Vlah <vlahm13@gmail.com>
2020-08-19 21:45:00 -0400

    detlim system fully distributed; untested

--- src/lter/hjandrews/retrieve.R
+++ src/lter/hjandrews/retrieve.R
@@ -2,7 +2,7 @@
 prod_info = get_product_info(network=network, domain=domain,
     status_level='retrieve', get_statuses='ready')
 
-# i=3
+# i=5
 for(i in 1:nrow(prod_info)){
 
     prodname_ms = glue(prod_info$prodname[i], '__', prod_info$prodcode[i])
