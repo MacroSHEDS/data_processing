dc8cb702085fa26bad9b9670f491523c2a5468f5
Mike Vlah <vlahm13@gmail.com>
2020-09-15 18:33:25 -0400

    adapted detection limit machinery for long format

--- src/templates/func_code_templates.R
+++ src/templates/func_code_templates.R
@@ -244,3 +244,69 @@ process_1_5482 <- function(network, domain, prodname_ms, site_name,
 
     return(d)
 }
+
+# initial munge operations ####
+# all of this is now taken care of by ms_read_raw_csv and ms_cast_and_reflag,
+#   but it might still be necessary to revisit in some cases
+
+    # d = sw(read_csv(rawfile1,
+    #                 progress = FALSE,
+    #                 col_types = readr::cols_only(
+    #                     DATE_TIME='c', SITECODE='c', TYPE='c', MEAN_LPS='d',
+    #                     PH='d', COND='d',
+    #                     ALK='d', SSED='d', SI='d', UTP='d', TDP='d', PARTP='d',
+    #                     PO4P='d', UTN='d', TDN='d', DON='d', PARTN='d', UTKN='d',
+    #                     TKN='d', NH3N='d', NO3N='d', `NA` = 'd', K='d', CA='d',
+    #                     MG='d', SO4S='d', CL='d', DOC='d', ANCA='d',
+    #                     #flux-only cols
+    #                     ALK_OUTPUT='d', SSED_OUTPUT='d', SI_OUTPUT='d',
+    #                     UTP_OUTPUT='d', TDP_OUTPUT='d', PARTP_OUTPUT='d',
+    #                     PO4P_OUTPUT='d', UTN_OUTPUT='d', TDN_OUTPUT='d',
+    #                     DON_OUTPUT='d', PARTN_OUTPUT='d', UTKN_OUTPUT='d',
+    #                     TKN_OUTPUT='d', NH3N_OUTPUT='d', NO3N_OUTPUT='d',
+    #                     NA_OUTPUT = 'd', K_OUTPUT='d', CA_OUTPUT='d',
+    #                     MG_OUTPUT='d', SO4S_OUTPUT='d', CL_OUTPUT='d',
+    #                     DOC_OUTPUT='d',
+    #                     #flag cols
+    #                     PHCODE='c', CONDCODE='c', ALKCODE='c',
+    #                     SSEDCODE='c', SICODE='c',
+    #                     UTPCODE='c', TDPCODE='c', PARTPCODE='c',
+    #                     PO4PCODE='c', UTNCODE='c',
+    #                     TDNCODE='c', DONCODE='c',
+    #                     PARTNCODE='c', UTKNCODE='c',
+    #                     TKNCODE='c', NH3NCODE='c',
+    #                     NO3NCODE='c', NACODE='c', KCODE='c',
+    #                     CACODE='c', MGCODE='c',
+    #                     SO4SCODE='c', CLCODE='c', DOCCODE='c',
+    #                     PVOLCODE='c', ANCACODE='c'))) %>%
+    #     filter(! TYPE  %in% c('N', 'S', 'YE', 'QB', 'QS', 'QL', 'QA')) %>%
+    #     rename(site_name = SITECODE,
+    #            datetime = DATE_TIME) %>%
+    #     rename_all(dplyr::recode,
+    #                #conc colnames
+    #                # MEAN_LPS='discharge_ns',
+    #                PH='pH', COND='spCond', ALK='alk',
+    #                SSED='suspSed', SI='Si', PARTP='TPP', PO4P='PO4_P',
+    #                PARTN='TPN', NH3N='NH3_N', NO3N='NO3_N', CA='Ca', MG='Mg',
+    #                SO4S='SO4_S', CL='Cl', ANCA='AnCaR', `NA`='Na',
+    #                #flux colnames
+    #                ALK_OUTPUT='alk',
+    #                SSED_OUTPUT='suspSed', SI_OUTPUT='Si', PARTP_OUTPUT='TPP',
+    #                PO4P_OUTPUT='PO4_P', PARTN_OUTPUT='TPN', NH3N_OUTPUT='NH3_N',
+    #                NO3N_OUTPUT='NO3_N', CA_OUTPUT='Ca', MG_OUTPUT='Mg',
+    #                SO4S_OUTPUT='SO4_S', CL_OUTPUT='Cl',
+    #                UTP_OUTPUT='UTP', TDP_OUTPUT='TDP', UTN_OUTPUT='UTN',
+    #                TDN_OUTPUT='TDN', DON_OUTPUT='DON', UTKN_OUTPUT='UTKN',
+    #                TKN_OUTPUT='TKN', NA_OUTPUT='Na', K_OUTPUT='K',
+    #                DOC_OUTPUT='DOC',
+    #                #varflag_colnames
+    #                PHCODE='pH!!!', CONDCODE='spCond!!!', ALKCODE='alk!!!',
+    #                SSEDCODE='suspSed!!!', SICODE='Si!!!',
+    #                PARTPCODE='TPP!!!', UTPCODE='UTP!!!'
+    #                PO4PCODE='PO4_P!!!',
+    #                PARTNCODE='TPN!!!',
+    #                NH3NCODE='NH3_N!!!',
+    #                NO3NCODE='NO3_N!!!', NACODE='Na!!!', KCODE='K!!!',
+    #                CACODE='Ca!!!', MGCODE='Mg!!!',
+    #                SO4SCODE='SO4_S!!!', CLCODE='Cl!!!', DOCCODE='DOC!!!',
+    #                ANCACODE='ANCA!!!') %>%
