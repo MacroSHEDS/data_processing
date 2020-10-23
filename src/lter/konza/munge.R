ca89fd3fddd4e75388cf63a06091bf3273c92983
Mike Vlah <vlahm13@gmail.com>
2020-10-23 17:42:04 -0400

    almost done with munge -> derive hardlinker

--- src/lter/konza/munge.R
+++ src/lter/konza/munge.R
@@ -3,7 +3,7 @@ loginfo('Beginning munge', logger=logger_module)
 prod_info = get_product_info(network=network, domain=domain,
                              status_level='munge', get_statuses='ready')
 
-# i=5
+# i=5; j=1
 for(i in 1:nrow(prod_info)){
     # for(i in 5){
 
@@ -19,7 +19,6 @@ for(i in 1:nrow(prod_info)){
 
     sites = names(held_data[[prodname_ms]])
 
-    # j <- 1
     for(j in 1:length(sites)){
 
         site_name <- sites[j]
