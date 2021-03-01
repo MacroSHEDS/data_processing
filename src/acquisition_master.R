1bfeac05f08471b813b4b291b6f8ceca5a8f3da2
Spencer Rhea <spencerrhea41@gmail.com>
2021-03-01 15:49:42 -0500

    Niwot precip correction factor for saddle site due to blowing snow

--- src/acquisition_master.R
+++ src/acquisition_master.R
@@ -219,7 +219,7 @@ ms_globals <- c(ls(all.names=TRUE), 'ms_globals')
 
 dir.create('logs', showWarnings = FALSE)
 
-# dmnrow=6
+# dmnrow=12
 for(dmnrow in 1:nrow(network_domain)){
 # for(dmnrow in 9){
     # drop_automated_entries('.') #use with caution!
