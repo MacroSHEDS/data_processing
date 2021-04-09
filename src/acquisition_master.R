75f54deaae020a7003cd0f059ecfd65c5bfd1ae7
Spencer Rhea <spencerrhea41@gmail.com>
2021-04-09 15:11:12 -0400

    nothing major

--- src/acquisition_master.R
+++ src/acquisition_master.R
@@ -214,7 +214,7 @@ ms_globals <- c(ls(all.names=TRUE), 'ms_globals')
 
 dir.create('logs', showWarnings = FALSE)
 
-# dmnrow=1
+# dmnrow=16
 for(dmnrow in 1:nrow(network_domain)){
 
     # drop_automated_entries('.') #use with caution!
