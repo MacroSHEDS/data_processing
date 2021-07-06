4f9d3a55bf55af33ae5b2fa433c74630f90dab20
spencerrhea <spenrhea@rams.colostate.edu>
2021-07-06 14:56:16 -0500

    little change

--- src/acquisition_master.R
+++ src/acquisition_master.R
@@ -220,7 +220,7 @@ ms_globals <- c(ls(all.names=TRUE), 'ms_globals')
 
 dir.create('logs', showWarnings = FALSE)
 
-# dmnrow=11
+# dmnrow=3
 for(dmnrow in 1:nrow(network_domain)){
 
     # drop_automated_entries('.') #use with caution!
