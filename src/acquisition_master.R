2a591c5f803279927d20d08fc24e8fcd0eb5265a
spencerrhea <spenrhea@rams.colostate.edu>
2021-08-03 09:37:39 -0400

    santee ws boundaries, ws area calc, ws101 removed

--- src/acquisition_master.R
+++ src/acquisition_master.R
@@ -239,7 +239,7 @@ ms_globals <- c(ls(all.names=TRUE), 'ms_globals')
 
 dir.create('logs', showWarnings = FALSE)
 
-# dmnrow = 16
+# dmnrow = 22
 # print(network_domain, n=50)
 # for(dmnrow in 1:nrow(network_domain)){
 for(dmnrow in 1:nrow(network_domain)){
@@ -276,17 +276,17 @@ for(dmnrow in 1:nrow(network_domain)){
                           domain = domain)
 
     ms_retrieve(network = network,
-                # prodname_filter = c('ws_boundary'),
+                prodname_filter = c('ws_boundary'),
                 domain = domain)
     ms_munge(network = network,
-             # prodname_filter = c('ws_boundary'),
+             prodname_filter = c('ws_boundary'),
              domain = domain)
     sw(ms_delineate(network = network,
                     domain = domain,
                     dev_machine_status = ms_instance$machine_status,
                     verbose = TRUE))
     ms_derive(network = network,
-              # prodname_filter = c('ws_boundary'),
+              prodname_filter = c('ws_boundary'),
               domain = domain)
     ms_general(network = network,
                domain = domain)
