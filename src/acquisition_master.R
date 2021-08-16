abe04d3d3b115c33f612fcb31c764ae4df207eab
Spencer Rhea <sr446@duke.edu>
2021-08-16 16:39:10 -0400

    east river update

--- src/acquisition_master.R
+++ src/acquisition_master.R
@@ -242,7 +242,7 @@ ms_globals <- c(ls(all.names = TRUE), 'ms_globals')
 
 dir.create('logs', showWarnings = FALSE)
 
-# dmnrow = 14
+# dmnrow = 5
 # print(network_domain, n=50)
 # for(dmnrow in 1:nrow(network_domain)){
 for(dmnrow in 1:nrow(network_domain)){
