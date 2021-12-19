059e1d69c2e5b35d53d59f80c5242870b3742cca
Spencer Rhea <spencerrhea41@gmail.com>
2021-12-19 14:04:12 -0500

    updates to identify_smapling

--- src/acquisition_master.R
+++ src/acquisition_master.R
@@ -253,10 +253,10 @@ ms_globals <- c(ls(all.names = TRUE), 'ms_globals')
 
 dir.create('logs', showWarnings = FALSE)
 
-# dmnrow = 2
+# dmnrow = 12
 # print(network_domain, n=50)
 # for(dmnrow in 1:nrow(network_domain)){
-for(dmnrow in 20:nrow(network_domain)){
+for(dmnrow in 1:nrow(network_domain)){
 
     # drop_automated_entries('.') #use with caution!
     # drop_automated_entries(glue('data/{n}/{d}', n = network, d = domain))
@@ -290,18 +290,18 @@ for(dmnrow in 20:nrow(network_domain)){
                          domain = domain)
 
     ms_retrieve(network = network,
-                #prodname_filter = c('discharge'),
+                # prodname_filter = c('stream_chemistry'),
                 domain = domain)
     ms_munge(network = network,
-             prodname_filter = c('ws_boundary'),
+             # prodname_filter = c('stream_chemistry'),
              domain = domain)
-    # sw(ms_delineate(network = network,
-    #                 domain = domain,
-    #                 dev_machine_status = ms_instance$machine_status,
-    #                 verbose = TRUE))
-    # ms_derive(network = network,
-              # prodname_filter = c('ws_boundary'),
-    #           domain = domain)
+    sw(ms_delineate(network = network,
+                    domain = domain,
+                    dev_machine_status = ms_instance$machine_status,
+                    verbose = TRUE))
+    ms_derive(network = network,
+              prodname_filter = c('stream_chemistry'),
+              domain = domain)
     ms_general(network = network,
                domain = domain)
 
