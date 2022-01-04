3080cd842d0d2c30bd25fb044e43515eebd1826d
Spencer Rhea <spencerrhea41@gmail.com>
2022-01-04 17:59:27 -0500

    update to data pipline

--- src/acquisition_master.R
+++ src/acquisition_master.R
@@ -253,7 +253,7 @@ ms_globals <- c(ls(all.names = TRUE), 'ms_globals')
 
 dir.create('logs', showWarnings = FALSE)
 
-# dmnrow = 23
+# dmnrow = 24
 # print(network_domain, n=50)
 # for(dmnrow in 1:nrow(network_domain)){
 for(dmnrow in 1:nrow(network_domain)){
