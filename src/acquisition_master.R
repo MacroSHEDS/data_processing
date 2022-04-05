8f61e70011f8acb6ecab34eb3e0b26b48ef71cda
Spencer Rhea <spencerrhea41@gmail.com>
2022-04-05 10:07:03 -0400

    domain updates/fixes

--- src/acquisition_master.R
+++ src/acquisition_master.R
@@ -253,7 +253,7 @@ ms_globals <- c(ls(all.names = TRUE), 'ms_globals')
 
 dir.create('logs', showWarnings = FALSE)
 
-# dmnrow = 3
+# dmnrow = 9
 # print(network_domain, n=50)
 # for(dmnrow in 1:nrow(network_domain)){
 for(dmnrow in 1:nrow(network_domain)){
