8532f47bdb47bf73d5547cbf97ebdefd49359cea
Mike Vlah <vlahm13@gmail.com>
2022-10-28 18:02:38 -0600

    data fully restructured. just making metadata now

--- src/global/one-off/build_eml_templates.R
+++ src/global/one-off/build_eml_templates.R
@@ -1,14 +1,21 @@
 #don't source this file. it was used in the creation of macrosheds/data_processing/eml,
 #but everything in there should now be edited manually or piecemeal
 
+stop('if this is for macrosheds > v1, generalize paths')
+
 library(EMLassemblyline)
+library(tidyverse)
 
 setwd('~/git/macrosheds/data_acquisition')
+
+conf <- jsonlite::fromJSON('config.json',
+                           simplifyDataFrame = FALSE)
 
 wd <- file.path('eml', 'eml_templates')
 ed <- file.path('eml', 'eml_out')
 dd <- file.path('eml', 'data_links')
 
+unlink(dd, recursive = TRUE)
 dir.create(wd, recursive = TRUE)
 dir.create(ed, recursive = TRUE)
 dir.create(dd, recursive = TRUE)
@@ -42,31 +49,58 @@ template_annotations()
 
 # template_arguments()
 
-files_to_link <- c('macrosheds_figshare_v1/1_watershed_attribute_data/ws_attr_summaries.csv',
-                   'macrosheds_figshare_v1/2_timeseries_data/bear/bear/discharge/EB.csv')
+ts_tables <- list.files('macrosheds_figshare_v1/2_timeseries_data', pattern = '\\.csv$',
+                        recursive = TRUE, full.names = TRUE)
+
+files_to_link <- c(ts_tables,
+                   list.files('macrosheds_figshare_v1/1_watershed_attribute_data',
+                              full.names = TRUE, recursive = TRUE),
+                   'macrosheds_figshare_v1/3_CAMELS-compliant_watershed_attributes/CAMELS_compliant_ws_attr.csv',
+                   'macrosheds_figshare_v1/4_CAMELS-compliant_Daymet_forcings/CAMELS_compliant_Daymet_forcings.csv')
 basenames <- basename(files_to_link)
 link_locs <- file.path(dd, basenames)
+
+descriptions <- basenames
+descriptions <- str_replace(descriptions,
+                            '^timeseries_([a-z_]+)\\.csv$',
+                            'Time-series (streamflow, precip if available, chemistry) for domain: \\1')
+descriptions <- str_replace(descriptions,
+                            '^ws_attr_summaries\\.csv$',
+                            'Watershed attribute data, summarized across time, for all domains')
+descriptions <- str_replace(descriptions,
+                            '^ws_attr_timeseries\\.csv$',
+                            'Watershed attribute data, temporally explicit, for all domains')
+descriptions <- str_replace(descriptions,
+                            '^CAMELS_compliant_ws_attr\\.csv$',
+                            'Watershed attribute data, temporally explicit, for all domains, and interoperable with the CAMELS dataset (https://ral.ucar.edu/solutions/products/camels)')
+descriptions <- str_replace(descriptions,
+                            '^CAMELS_compliant_Daymet_forcings\\.csv$',
+                            'Daymet climate forcings for all domains; interoperable with the CAMELS dataset (https://ral.ucar.edu/solutions/products/camels)')
 
 for(i in seq_along(files_to_link)){
     suppressWarnings(file.link(files_to_link[i], link_locs[i]))
 }
 
+
+# temporal_coverage <- map(ts_tables, ~range(read_csv(.)$datetime)) %>%
+#     reduce(~c(min(c(.x[1], .y[1])), max(c(.x[2], .y[2]))))
+temporal_coverage <- c("1945-07-01", "2022-04-16")
+
 make_eml(wd, dd, ed,
          dataset.title = 'MacroSheds',
-         temporal.coverage = c('2012-05-01', '2014-11-30'),
+         temporal.coverage = as.Date(temporal_coverage),
          geographic.description = NULL,#not needed if geographic_coverage.txt exists,
          geographic.coordinates = NULL,#same,
          maintenance.description = 'ongoing',
          data.table = basenames,
-         # data.table.name = data.table, #takes care of itself
-         data.table.description = rep('placeholder', length(files_to_link)),
+         # data.table.name = data.table,
+         data.table.description = descriptions,
          data.table.quote.character = rep('"', length(files_to_link)),
          data.table.url = NULL,
-         other.entity = NULL, #list any non-table (zip, shp, R, etc) file here
+         other.entity = 'eml/data_links/shapefiles.zip',
          # other.entity.name = other.entity,
-         other.entity.description = NULL,
+         other.entity.description = 'Watershed boundaries, stream gauge locations, and precip gauge locations, for all domains',
          other.entity.url = NULL,
-         # provenance = c('knb-lter-cap.46.3'), #deprecated. use template_provenance()
-         user.id = NULL,
-         user.domain = NULL, #pretty sure this doesn't apply to me
+         user.id = conf$edi_user_id,
+         user.domain = NULL, #pretty sure this doesn't apply to us
          package.id = NULL)
