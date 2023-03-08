2df2090ab5fe81a268d4c8f14923a0b123ecacb4
weston <westonslaughter@gmail.com>
2023-03-08 16:30:47 -0500

    stream chem pkernel up to variable units work

--- src/mces/mces/domain_helpers.R
+++ src/mces/mces/domain_helpers.R
@@ -6,6 +6,18 @@ utm_to_wsg <- function(x, y) {
   return(lonlat)
 }
 
+mces_site_lookup <- function(site_string) {
+  tryCatch(
+    expr = {
+      site_code <- mces_site_codes[grepl(site_string, names(mces_site_codes))][[1]]
+      return(site_code)
+    },
+    error = function(e) {
+      return(site_string)
+    }
+  )
+}
+
 ## ## UTM x and y from MCES data
 ## lat_longs <- list(
 ##   c(476393.07, 4980372.46),
@@ -93,3 +105,87 @@ retrieve_mces_product <- function(network,
                      prodname_ms = prodname_ms)
 
 }
+
+mces_variable_info <- list(
+"Total Kjeldahl Nitrogen, Unfiltered" = c('', '', ''),
+"Total Phosphorus, Unfiltered" = c('', '', ''),
+"Volatile Suspended Solids" = c('', '', ''),
+"Ortho Phosphate as P, Filtered" = c('', '', ''),
+"Total Phosphorus, Filtered" = c('', '', ''),
+"Chlorophyll-a, % Pheo-Corrected" = c('', '', ''),
+"Chlorophyll-a/Pheophytin-a Abs. R" = c('', '', ''),
+"Chlorophyll-a Trichromatic Uncorrected" = c('', '', ''),
+"Chlorophyll-a, Pheo-Corrected" = c('', '', ''),
+"Suspended Solids" = c('', '', ''),
+"Total Nitrate/Nitrite N, Unfiltered" = c('', '', ''),
+"Nitrite N, Unfiltered" = c('', '', ''),
+"Nitrate N, Unfiltered" = c('', '', ''),
+"E. Coli Bacteria Count" = c('', '', ''),
+"Chloride, Unfiltered" = c('', '', ''),
+"Ammonia Nitrogen, Unfiltered" = c('', '', ''),
+"Chlorophyll-b" = c('', '', ''),
+"Chlorophyll-c" = c('', '', ''),
+"Pheophytin-a" = c('', '', ''),
+"Conductivity" = c('', '', ''),
+"Dissolved Oxygen" = c('', '', ''),
+"pH" = c('', '', ''),
+"Temperature" = c('', '', ''),
+"Cadmium, Unfiltered" = c('', '', ''),
+"Turbidity (NTRU)" = c('', '', ''),
+"Total Organic Carbon, Filtered" = c('', '', ''),
+"Turbidity (NTU)" = c('', '', ''),
+"COD, Unfiltered" = c('', '', ''),
+"Hardness, Unfiltered" = c('', '', ''),
+"Lead, Unfiltered" = c('', '', ''),
+"Sulfate, Unfiltered" = c('', '', ''),
+"Ortho Phosphate as P, Unfiltered" = c('', '', ''),
+"Silica, Filtered" = c('', '', ''),
+"Total Alkalinity, Unfiltered" = c('', '', ''),
+"Zinc, Unfiltered" = c('', '', ''),
+"Fecal Coliform Bacteria Count" = c('', '', ''),
+"Copper, Unfiltered" = c('', '', ''),
+"Total Kjeldahl Nitrogen, Filtered" = c('', '', ''),
+"Chromium, Unfiltered" = c('', '', ''),
+"Magnesium, Unfiltered" = c('', '', ''),
+"COD, Filtered" = c('', '', ''),
+"Nickel, Unfiltered" = c('', '', ''),
+"CBOD 5-day, Unfiltered" = c('', '', ''),
+"BOD 5-day, Unfiltered" = c('', '', ''),
+"Sulfate, Filtered" = c('', '', ''),
+"BOD K-rate, Unfiltered" = c('', '', ''),
+"BOD Ultimate, Filtered" = c('', '', ''),
+"BOD K-rate, Filtered" = c('', '', ''),
+"Total Phosphorus, Filtered, Low L" = c('', '', ''),
+"Soluble Oxygen Demand, Filtered" = c('', '', ''),
+"Total Dissolved Solids" = c('', '', ''),
+"Calcium, Unfiltered" = c('', '', ''),
+"Cadmium, Filtered" = c('', '', ''),
+"Total Phosphorus, Unfiltered", Low = c('', '', ''),
+"Transparency Tube" = c('', '', ''),
+"Copper, Filtered" = c('', '', ''),
+"Chromium, Filtered" = c('', '', ''),
+"PCB: 1248, Unfiltered" = c('', '', ''),
+"Total Kjeldahl Nitrogen, Unfiltered" = c('', '', ''),
+"Mercury, Filtered" = c('', '', ''),
+"CBOD 5-day, Filtered" = c('', '', ''),
+"CBOD K-rate, Unfiltered" = c('', '', ''),
+"CBOD K-rate, Filtered" = c('', '', ''),
+"Ammonia Nitrogen, Filtered" = c('', '', ''),
+"Zinc, Filtered" = c('', '', ''),
+"Total Alkalinity, Filtered" = c('', '', ''),
+"Sodium, Filtered" = c('', '', ''),
+"CBOD Ultimate, Unfiltered" = c('', '', ''),
+"PCB: 1232, Unfiltered" = c('', '', ''),
+"BOD Ultimate, Unfiltered" = c('', '', ''),
+"Mercury, Unfiltered" = c('', '', ''),
+"PCB: 1016, Unfiltered" = c('', '', ''),
+"Turbidity (FNU)" = c('', '', ''),
+"BOD 5-day, Filtered" = c('', '', ''),
+"Chloride, Filtered" = c('', '', ''),
+"CBOD Ultimate, Filtered" = c('', '', ''),
+"Total Phosphorus, Particulate" = c('', '', ''),
+"Magnesium, Filtered" = c('', '', ''),
+"PCB: 1254, Unfiltered" = c('', '', ''),
+"PCB: 1221, Unfiltered" = c('', '', ''),
+"Potassium, Filtered" = c('', '', ''),
+"Total Organic Carbon, Unfiltered" = c('', '', ''))
