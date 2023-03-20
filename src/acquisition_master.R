6361ea011dcb14799082f34de9de206ec7060186
weston <westonslaughter@gmail.com>
2023-03-20 16:36:49 -0400

    MWO SWWD watershed delin specs

--- src/acquisition_master.R
+++ src/acquisition_master.R
@@ -340,7 +340,7 @@ dir.create('logs', showWarnings = FALSE)
 ## scrape_data_download_urls()
 
 ## change string in line below to find row index of your desired domain
-## dmnrow <- which(network_domain$domain == 'swwd')
+dmnrow <- which(network_domain$domain == 'swwd')
 ## network_domain=filter(network_domain, ! network %in% c('lter', 'webb', 'mwo', 'neon'))
 for(dmnrow in 1:nrow(network_domain)){
 
@@ -393,7 +393,7 @@ for(dmnrow in 1:nrow(network_domain)){
         sw(ms_delineate(network = network,
                         domain = domain,
                         dev_machine_status = ms_instance$machine_status,
-                        ## overwrite_wb_sites = sites_ws_exclude[1],
+                        overwrite_wb_sites = "st-paul-park",
                         verbose = TRUE
                         ))
     }
