022110d055870ee5e5f6e55fa7419392aacf4fe5
weston <westonslaughter@gmail.com>
2023-04-06 10:22:56 -0400

    work on pkernels

--- src/acquisition_master.R
+++ src/acquisition_master.R
@@ -323,7 +323,7 @@ dir.create('logs', showWarnings = FALSE)
 ## scrape_data_download_urls()
 
 ## change string in line below to find row index of your desired domain
-dmnrow <- which(network_domain$domain == 'mces')
+dmnrow <- which(network_domain$domain == 'sleepers')
 
 ## network_domain=filter(network_domain, ! network %in% c('lter', 'webb', 'mwo', 'neon'))
 for(dmnrow in 1:nrow(network_domain)){
@@ -383,7 +383,7 @@ for(dmnrow in 1:nrow(network_domain)){
     }
 
     ms_derive(network = network,
-              # prodname_filter = c('stream_chemistry'),
+              prodname_filter = c('discharge'),
               domain = domain)
 
     if(domain != 'mcmurdo'){
