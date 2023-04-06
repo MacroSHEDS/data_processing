fe3cef529523a5d9961a91f3b818f4d073c1470d
weston <westonslaughter@gmail.com>
2023-04-06 16:31:53 -0400

    half-bake on dl auto-updater from dl info embedded in raw data

--- src/acquisition_master.R
+++ src/acquisition_master.R
@@ -308,13 +308,6 @@ superunknowns <- get_superunknowns(special_vars = c('discharge', 'precipitation'
 site_data <- filter(site_data,
                     as.logical(in_workflow))
 
-# for run 2023_04_03
-site_data_bk <- site_data
-site_data <- site_data %>%
-  filter(
-    !domain %in% c("loch_vale", "panola", "trout_lake", "ecnhc")
-  )
-
 network_domain <- site_data %>%
     select(network, domain) %>%
     distinct() %>%
@@ -330,7 +323,7 @@ dir.create('logs', showWarnings = FALSE)
 ## scrape_data_download_urls()
 
 ## change string in line below to find row index of your desired domain
-dmnrow <- which(network_domain$domain == 'sleepers')
+dmnrow <- which(network_domain$domain == 'mces')
 
 ## network_domain=filter(network_domain, ! network %in% c('lter', 'webb', 'mwo', 'neon'))
 for(dmnrow in 1:nrow(network_domain)){
