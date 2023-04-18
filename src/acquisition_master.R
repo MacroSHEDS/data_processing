35d7d58562f012eeb3b0c423145851cd2fd93ee5
weston <westonslaughter@gmail.com>
2023-04-18 09:33:20 -0400

    small edits

--- src/acquisition_master.R
+++ src/acquisition_master.R
@@ -323,7 +323,7 @@ dir.create('logs', showWarnings = FALSE)
 ## scrape_data_download_urls()
 
 ## change string in line below to find row index of your desired domain
-dmnrow <- which(network_domain$domain == 'mces')
+dmnrow <- which(network_domain$domain == 'sleepers')
 
 ## network_domain=filter(network_domain, ! network %in% c('lter', 'webb', 'mwo', 'neon'))
 for(dmnrow in 1:nrow(network_domain)){
