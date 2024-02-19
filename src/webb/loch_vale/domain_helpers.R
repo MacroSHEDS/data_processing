57898ea6db0997dbaa4128a2716e8a859d81b910
Mike Vlah <vlahm13@gmail.com>
2024-02-19 11:28:51 -0700

    encapsulated wqp retrieval and munge

--- src/webb/loch_vale/domain_helpers.R
+++ src/webb/loch_vale/domain_helpers.R
@@ -6,4 +6,147 @@ wqp_codes <- tibble(
    priority =   c(1 ,          1 ,       1 ,      1 ,         2 ,      1,       3,       1 ,      1 ,      1 ,          1 ,      1 ,      1 ,      1 ,      1 ,      1 ,      1 ,      1 ,      1 ,      1 ,      1 ,      1 ,      1,       1 ,      1 ,          1,       2,       1 ,         1 ,      1 ,      1 ,          1 ,       1 ,       1 ,       1 ,         1 ,      1 ,      1 ,      1 ,      1 ,      1 ,      1 ,      2,       1 ,       1 ,       1 ,      1 ,    1,       1)
 )
 
-# table(wqp_codes$ms_varcode)
+retrieve_wqp_chem <- function(nwis_site_code, siteparams, set_details){
+
+    raw_data_dest <- glue('data/{n}/{d}/raw/{p}/{s}',
+                          n = network,
+                          d = domain,
+                          p = set_details$prodname_ms,
+                          s = set_details$site_code)
+
+    dir.create(path = raw_data_dest,
+               showWarnings = FALSE,
+               recursive = TRUE)
+
+    siteparams <- siteparams %>%
+        inner_join(wqp_codes, by = c(parm_cd = 'param_code')) %>%
+        group_by(ms_varcode) %>%
+        filter(priority == min(priority)) %>%
+        ungroup() %>%
+        distinct(parm_cd) %>%
+        pull(parm_cd)
+
+    siteWQ <- readWQPqw(siteNumbers = paste0('USGS-', nwis_site_code),
+                        parameterCd = siteparams)
+
+    rawfile <- glue('{rd}/{c}.csv',
+                    rd = raw_data_dest,
+                    c = set_details$component)
+
+    write_csv(siteWQ, file = rawfile)
+}
+
+process_wqp_chem <- function(rawfile, ms_site_code){
+
+    d <- read.delim(rawfile, sep = ',', colClasses = 'character') %>%
+        as_tibble()
+
+    pcodes_present <- unique(d$USGSPCode)
+
+    if(! all(na.omit(d$ResultMeasure.MeasureUnitCode == d$DetectionQuantitationLimitMeasure.MeasureUnitCode))){
+        stop('need to account for result/dl unit mismatch')
+    }
+
+    if(any(! is.na(d$MeasureQualifierCode))){
+        stop('evaluate contents of MeasureQualifierCode')
+    }
+
+    comment_redflags <- c('exceed', 'improper', 'contaminat', 'error',
+                          'subsample', 'violat', 'fail', 'invalid', 'questionable',
+                          'suspect', 'adjust', 'interfere')
+
+    d <- d %>%
+        # select(DetectionQuantitationLimitMeasure.MeasureValue, #dl
+        #        ResultDetectionConditionText, #detected/not-detected
+        #        # MeasureQualifierCode, #comment/flag?
+        #        ResultValueTypeName, #estimated/actual
+        #        ResultCommentText, #free form
+        #        ResultStatusIdentifier, #accepted/historical/preliminary
+        #        ResultMeasureValue, #data
+        #        ActivityStartDateTime, #datetime UTC
+        #        USGSPCode) %>% #parameter code
+        mutate(ms_status_ = case_when
+               (
+                   ResultStatusIdentifier == 'Preliminary' ~ 1,
+                   ResultValueTypeName == 'Estimated' ~ 1,
+                   grepl(paste(comment_redflags, collapse = '|'),
+                         ResultCommentText) ~ 1,
+                   TRUE ~ 0
+               ),
+               ResultMeasureValue = if_else
+               (
+                   ! is.na(ResultDetectionConditionText) & ResultDetectionConditionText == 'Not Detected',
+                   paste0('<', DetectionQuantitationLimitMeasure.MeasureValue),
+                   ResultMeasureValue
+               ),
+               site_code = ms_site_code
+        ) %>%
+        inner_join(wqp_codes, by = c(USGSPCode = 'param_code')) %>%
+        select(ResultMeasureValue,
+               ActivityStartDateTime,
+               ms_varcode,
+               ms_status_,
+               site_code)
+
+    if(any(is.na(d$ms_status_))) stop('oi')
+    z = select(d, ResultMeasureValue) %>% mutate(ResultMeasureValue = sub('<', '', ResultMeasureValue))
+    if(length(get_nonnumerics(z))) stop('codes in data column')
+
+    d_wide <- pivot_wider(d,
+                          names_from = ms_varcode,
+                          values_from = ResultMeasureValue)
+
+    d <- ms_read_raw_csv(preprocessed_tibble = d_wide,
+                         datetime_cols = c(ActivityStartDateTime = '%Y-%m-%dT%H:%M:%S'),
+                         datetime_tz = 'GMT',
+                         site_code_col = 'site_code',
+                         data_cols = unique(d$ms_varcode),
+                         data_col_pattern = '#V#',
+                         is_sensor = FALSE,
+                         convert_to_BDL_flag = '<#*#',
+                         summary_flagcols = 'ms_status_')
+
+    d <- ms_cast_and_reflag(d,
+                            summary_flags_clean = list(ms_status_ = '0'),
+                            summary_flags_dirty = list(ms_status_ = '1'),
+                            variable_flags_bdl = 'BDL')
+
+    #if e.g. NH4 and NH4_N are reported, just keep the latter.
+    conv_cules <- c('NO3', 'SO4', 'PO4', 'SiO2', 'SiO3', 'NH4', 'NH3', 'NO3_NO2')
+    conv_cules_main <- paste(conv_cules,
+                             substr(conv_cules, 1, 1),
+                             sep = '_')
+    redundants <- which(conv_cules %in% drop_var_prefix(d$var) &
+                            sapply(conv_cules_main, function(x) any(grepl(x, d$var)))) %>%
+        names() %>%
+        str_extract('^(.+)(?=_\\w+$)')
+
+    d <- filter(d, ! drop_var_prefix(var) %in% redundants)
+
+    #handle necessary conversions
+    required_conversions <- wqp_codes %>%
+        filter(param_code %in% pcodes_present) %>%
+        left_join(ms_vars,
+                  by = c('ms_varcode' = 'variable_code'),
+                  suffix = c('_usgs', '_ms')) %>%
+        select(ms_varcode, starts_with('unit')) %>%
+        filter(unit_ms != unit_usgs)
+
+    d <- ms_conversions(d,
+                        convert_units_from = setNames(required_conversions$unit_usgs,
+                                                      required_conversions$ms_varcode),
+                        convert_units_to = setNames(required_conversions$unit_ms,
+                                                    required_conversions$ms_varcode))
+
+    d <- qc_hdetlim_and_uncert(d, prodname_ms = prodname_ms)
+
+    d <- synchronize_timestep(d)
+
+    write_ms_file(d = d,
+                  network = network,
+                  domain = domain,
+                  prodname_ms = prodname_ms,
+                  site_code = ms_site_code,
+                  level = 'munged',
+                  shapefile = FALSE)
+}
--- src/webb/loch_vale/processing_kernels.R
+++ src/webb/loch_vale/processing_kernels.R
@@ -60,49 +60,20 @@ process_0_VERSIONLESS002 <-  function(network, domain, set_details){
 #. handle_errors
 process_0_VERSIONLESS006 <- function(network, domain, set_details){
 
-    raw_data_dest <- glue('data/{n}/{d}/raw/{p}/{s}',
-                          n = network,
-                          d = domain,
-                          p = set_details$prodname_ms,
-                          s = set_details$site_code)
-
-    dir.create(path = raw_data_dest,
-               showWarnings = FALSE,
-               recursive = TRUE)
-
-    siteNumb <- "401723105400000"
+    nwis_site_code <- '401723105400000'
 
     #next time: use this chunk to identify params we might want.
     #if there are new ones, add them to wqp_codes (currently in loch_vale domain_helpers.R)
     #and then make access to wqp_codes more universal.
-    siteparams <- dataRetrieval::whatNWISdata(siteNumber = siteNumb) %>%
+    siteparams <- dataRetrieval::whatNWISdata(siteNumber = nwis_site_code) %>%
         as_tibble() %>%
         filter(count_nu > 10,
                ! is.na(parm_cd))
 
-    #use this to lookup parameter metadata
-    # zz = readNWISpCode(unique(siteparams$parm_cd))
+    #use this to cross-reference parameter metadata
+    View(readNWISpCode(unique(siteparams$parm_cd)))
 
-    #once you've modified wqp_codes, keep going.
-    #btw, if there is a next time, this whole procedure needs to be encapsulated
-    #and a more thorough look through the potential codes must be done.
-    #!! SEE process_0_VERSIONLESS008 for more thorough handling
-    siteparams <- siteparams %>%
-        inner_join(wqp_codes, by = c(parm_cd = 'param_code')) %>%
-        group_by(ms_varcode) %>%
-        filter(priority == min(priority)) %>%
-        ungroup() %>%
-        distinct(parm_cd) %>%
-        pull(parm_cd)
-
-    siteWQ <- readWQPqw(siteNumbers = paste0('USGS-', siteNumb),
-                        parameterCd = siteparams)
-
-    rawfile <- glue('{rd}/{c}.csv',
-                    rd = raw_data_dest,
-                    c = set_details$component)
-
-    write_csv(siteWQ, file = rawfile)
+    retrieve_wqp_chem(nwis_site_code, siteparams, set_details)
 
     access_time <- with_tz(Sys.time(), tzone = 'UTC')
     deets_out <- list(url = 'retrieved via dataRetrieval::readWQPqw',
@@ -116,49 +87,27 @@ process_0_VERSIONLESS006 <- function(network, domain, set_details){
 #. handle_errors
 process_0_VERSIONLESS007 <- function(network, domain, set_details){
 
-    raw_data_dest <- glue('data/{n}/{d}/raw/{p}/{s}',
-                          n = network,
-                          d = domain,
-                          p = set_details$prodname_ms,
-                          s = set_details$site_code)
-
-    dir.create(path = raw_data_dest,
-               showWarnings = FALSE,
-               recursive = TRUE)
-
-    siteNumb <- "401707105395000"
+    nwis_site_code <- '401707105395000'
 
     #next time: use this chunk to identify params we might want.
     #if there are new ones, add them to wqp_codes (currently in loch_vale domain_helpers.R)
     #and then make access to wqp_codes more universal.
-    siteparams <- dataRetrieval::whatNWISdata(siteNumber = siteNumb) %>%
+    siteparams <- dataRetrieval::whatNWISdata(siteNumber = nwis_site_code) %>%
         as_tibble() %>%
         filter(count_nu > 10,
                ! is.na(parm_cd))
 
-    #use this to lookup parameter metadata
-    # zz = readNWISpCode(unique(siteparams$parm_cd))
+    #use this to cross-reference parameter metadata
+    View(readNWISpCode(unique(siteparams$parm_cd)))
 
-    #once you've modified wqp_codes, keep going.
-    #btw, if there is a next time, this whole procedure needs to be encapsulated
-    #and a more thorough look through the potential codes must be done.
-    #!! SEE process_0_VERSIONLESS008 for more thorough handling
-    siteparams <- siteparams %>%
-        inner_join(wqp_codes, by = c(parm_cd = 'param_code')) %>%
-        group_by(ms_varcode) %>%
-        filter(priority == min(priority)) %>%
-        ungroup() %>%
-        distinct(parm_cd) %>%
-        pull(parm_cd)
+    retrieve_wqp_chem(nwis_site_code, siteparams, set_details)
 
-    siteWQ <- readWQPqw(siteNumbers = paste0('USGS-', siteNumb),
-                        parameterCd = siteparams)
+    access_time <- with_tz(Sys.time(), tzone = 'UTC')
+    deets_out <- list(url = 'retrieved via dataRetrieval::readWQPqw',
+                      access_time = as.character(access_time),
+                      last_mod_dt = access_time)
 
-    rawfile <- glue('{rd}/{c}.csv',
-                    rd = raw_data_dest,
-                    c = set_details$component)
-
-    write_csv(siteWQ, file = rawfile)
+    return(deets_out)
 
     access_time <- with_tz(Sys.time(), tzone = 'UTC')
     deets_out <- list(url = 'retrieved via dataRetrieval::readWQPqw',
@@ -172,48 +121,20 @@ process_0_VERSIONLESS007 <- function(network, domain, set_details){
 #. handle_errors
 process_0_VERSIONLESS008 <- function(network, domain, set_details){
 
-    raw_data_dest <- glue('data/{n}/{d}/raw/{p}/{s}',
-                          n = network,
-                          d = domain,
-                          p = set_details$prodname_ms,
-                          s = set_details$site_code)
-
-    dir.create(path = raw_data_dest,
-               showWarnings = FALSE,
-               recursive = TRUE)
-
-    siteNumb <- "401733105392404"
+    nwis_site_code <- '401733105392404'
 
     #next time: use this chunk to identify params we might want.
     #if there are new ones, add them to wqp_codes (currently in loch_vale domain_helpers.R)
     #and then make access to wqp_codes more universal.
-    siteparams <- dataRetrieval::whatNWISdata(siteNumber = siteNumb) %>%
+    siteparams <- dataRetrieval::whatNWISdata(siteNumber = nwis_site_code) %>%
         as_tibble() %>%
         filter(count_nu > 10,
                ! is.na(parm_cd))
 
-    #use this to look up parameter metadata
-    # zz = readNWISpCode(unique(siteparams$parm_cd))
+    #use this to cross-reference parameter metadata
+    View(readNWISpCode(unique(siteparams$parm_cd)))
 
-    #once you've modified wqp_codes, keep going.
-    #btw, if there is a next time, this whole procedure needs to be encapsulated
-    #and a more thorough look through the potential codes must be done
-    siteparams <- siteparams %>%
-        inner_join(wqp_codes, by = c(parm_cd = 'param_code')) %>%
-        group_by(ms_varcode) %>%
-        filter(priority == min(priority)) %>%
-        ungroup() %>%
-        distinct(parm_cd) %>%
-        pull(parm_cd)
-
-    siteWQ <- readWQPqw(siteNumbers = paste0('USGS-', siteNumb),
-                        parameterCd = siteparams)
-
-    rawfile <- glue('{rd}/{c}.csv',
-                    rd = raw_data_dest,
-                    c = set_details$component)
-
-    write_csv(siteWQ, file = rawfile)
+    retrieve_wqp_chem(nwis_site_code, siteparams, set_details)
 
     access_time <- with_tz(Sys.time(), tzone = 'UTC')
     deets_out <- list(url = 'retrieved via dataRetrieval::readWQPqw',
@@ -340,116 +261,9 @@ process_1_VERSIONLESS006 <- function(network, domain, prodname_ms, site_code, co
                     s = site_code,
                     c = component)
 
-    # d <- read.delim(rawfile, sep = ',', colClasses = 'character') %>%
-    #     as_tibble()
+    process_wqp_chem(rawfile = rawfile,
+                     ms_site_code = 'andrews_creek')
 
-    pcodes_present <- unique(d$USGSPCode)
-
-    if(! all(na.omit(d$ResultMeasure.MeasureUnitCode == d$DetectionQuantitationLimitMeasure.MeasureUnitCode))){
-        stop('need to account for result/dl unit mismatch')
-    }
-
-    if(any(! is.na(d$MeasureQualifierCode))){
-        stop('evaluate contents of MeasureQualifierCode')
-    }
-
-    comment_redflags <- c('exceed', 'improper', 'contaminat', 'error',
-                          'subsample', 'violat', 'fail', 'invalid', 'questionable',
-                          'suspect', 'adjust', 'interfere')
-
-    d <- d %>%
-        # select(DetectionQuantitationLimitMeasure.MeasureValue, #dl
-        #        ResultDetectionConditionText, #detected/not-detected
-        #        # MeasureQualifierCode, #comment/flag?
-        #        ResultValueTypeName, #estimated/actual
-        #        ResultCommentText, #free form
-        #        ResultStatusIdentifier, #accepted/historical/preliminary
-        #        ResultMeasureValue, #data
-        #        ActivityStartDateTime, #datetime UTC
-        #        USGSPCode) %>% #parameter code
-        mutate(ms_status_ = case_when
-               (
-                   ResultStatusIdentifier == 'Preliminary' ~ 1,
-                   ResultValueTypeName == 'Estimated' ~ 1,
-                   grepl(paste(comment_redflags, collapse = '|'),
-                         ResultCommentText) ~ 1,
-                   TRUE ~ 0
-               ),
-               ResultMeasureValue = if_else
-               (
-                   ! is.na(ResultDetectionConditionText) & ResultDetectionConditionText == 'Not Detected',
-                   paste0('<', DetectionQuantitationLimitMeasure.MeasureValue),
-                   ResultMeasureValue
-               ),
-               site_code = 'andrews_creek'
-               # datetime = str_extract(ActivityStartDateTime, '^([^\\.]+)')
-        ) %>%
-        inner_join(wqp_codes, by = c(USGSPCode = 'param_code')) %>%
-        select(ResultMeasureValue,
-               ActivityStartDateTime,
-               ms_varcode,
-               ms_status_,
-               site_code)
-
-    if(any(is.na(d$ms_status_))) stop('oi')
-    z = select(d, ResultMeasureValue) %>% mutate(ResultMeasureValue = sub('<', '', ResultMeasureValue))
-    if(length(get_nonnumerics(z))) stop('codes in data column')
-
-    d_wide <- pivot_wider(d,
-                          names_from = ms_varcode,
-                          values_from = ResultMeasureValue)
-
-    d <- ms_read_raw_csv(preprocessed_tibble = d_wide,
-                         datetime_cols = c(ActivityStartDateTime = '%Y-%m-%dT%H:%M:%S'),
-                         datetime_tz = 'GMT',
-                         site_code_col = 'site_code',
-                         data_cols = unique(d$ms_varcode),
-                         data_col_pattern = '#V#',
-                         is_sensor = FALSE,
-                         convert_to_BDL_flag = '<#*#',
-                         summary_flagcols = 'ms_status_')
-
-    d <- ms_cast_and_reflag(d,
-                            summary_flags_clean = list(ms_status_ = '0'),
-                            summary_flags_dirty = list(ms_status_ = '1'),
-                            variable_flags_bdl = 'BDL')
-
-    #ergh. if "-N' form is present, filter out molec form. same for all transformables
-
-    required_conversions <- wqp_codes %>%
-        filter(param_code %in% pcodes_present) %>%
-        left_join(ms_vars,
-                  by = c('ms_varcode' = 'variable_code'),
-                  suffix = c('_usgs', '_ms')) %>%
-        select(ms_varcode, starts_with('unit')) %>%
-        filter(unit_ms != unit_usgs)
-
-    #still gotta make sure NH4 doesn't convert?
-    d <- ms_conversions(d,
-                        convert_units_from = setNames(required_conversions$unit_usgs,
-                                                      required_conversions$ms_varcode),
-                        convert_units_to = setNames(required_conversions$unit_ms,
-                                                    required_conversions$ms_varcode))
-
-    d <- qc_hdetlim_and_uncert(d, prodname_ms = prodname_ms)
-
-    d <- synchronize_timestep(d)
-
-    sites <- unique(d$site_code)
-
-    for(s in 1:length(sites)){
-
-        d_site <- d %>%
-            filter(site_code == !!sites[s])
-
-        write_ms_file(d = d_site,
-                      network = network,
-                      domain = domain,
-                      prodname_ms = prodname_ms,
-                      site_code = sites[s],
-                      level = 'munged',
-                      shapefile = FALSE)
-    }
     return()
 }
 
