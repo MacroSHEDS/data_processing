9af9c1da31ec7283577d690757b401c2775c5154
Mike Vlah <vlahm13@gmail.com>
2024-03-03 10:41:00 -0700

    ignoring swp files

--- src/dev/test/ms_cast_and_reflag.R
+++ src/dev/test/ms_cast_and_reflag.R
@@ -25,6 +25,22 @@ ms_cast_and_reflag(fff,
                    variable_flags_bdl='B'
 )
 
+ms_cast_and_reflag(fff,
+                   # summary_flags_clean = list(sum_flag=c('1', '2'), sum_flag2 = '3'),
+                   # summary_flags_dirty = list(sum_flag=c('3'), sum_flag2 = ''),
+                   # summary_flags_to_drop = list(sum_flag=c(''), sum_flag2 = c('1', '2')),
+                   summary_flags_clean = list(sum_flag='1'),
+                   summary_flags_dirty = list(sum_flag='2'),
+                   summary_flags_to_drop = list(sum_flag='3'),
+                   summary_flags_bdl = list(sum_flag=''),
+
+                   variable_flags_clean=c('A', 'X', 'Y', ''),
+                   # variable_flags_clean='A',
+                   variable_flags_dirty='C',
+                   variable_flags_to_drop='Z',
+                   variable_flags_bdl='B'
+)
+
 tibble(x = c(1, NA, 3), y = c(NA, NA, 3)) %>%
     # filter(x %in% 1:2)
     filter(! x %in% 1:2)
